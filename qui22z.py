76fea11cce94055de446c6f8f7c94f11b51c08c5
Shivfun99 <shivkumarmishra718@gmail.com>
2024-07-02 15:07:01 +0530

    Add files via upload

---
+++ qui22z.py
@@ -0,0 +1,28 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play:)")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("CORRECT")
+else: print("incorrect")
+
+answer = input("what is ur clg name? ")
+if answer == "vit":
+    print("CORRECT")
+else: print("fine")
+
+answer = input("what is your gf name? ")
+if answer == "i am single":
+    print("CORRECT")
+else: print("wow")
+
+answer = input("what is ur age? ")
+if answer == "ninteen":
+    print("CORRECT")
+else: print("alr")
